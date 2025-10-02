6dc4345d2f27f3d380a02d84ff0ab76bdd2936dc
klingaje <149473210+klingaje@users.noreply.github.com>
2025-10-02 16:28:44 +0300

    enkel ws connection

---
+++ server.js
@@ -0,0 +1,45 @@
+const WebSocket = require('ws')
+const os = require('os')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+const wss = new WebSocket.Server({ port: PORT })
+
+const clients = new Set();
+
+wss.on('connection', (ws, req) => {
+    console.log('Client connected')
+
+
+    const urlParams = new URLSearchParams(req.url.slice(1))
+    if (urlParams.get('token') !== process.env.TOKEN) {
+        console.log('invalid token: ' + urlParams.get('token'))
+        ws.send(JSON.stringify({
+            status: 1,
+            msg: 'ERROR: Invalid token'
+        }))
+        ws.close();
+    }
+
+    if (!clients.has(ws)) {
+        clients.add(ws)
+    }
+
+    ws.on('message', (message) => {
+        console.log('Recieved message: ', message)
+
+        clients.forEach(client => {
+            client.send(JSON.stringify({
+                status: 0,
+                msg: String(message),
+
+            }))
+        })
+
+    })
+
+    ws.on('close', () => {
+        console.log('client disconnected')
+    })
+
+})
